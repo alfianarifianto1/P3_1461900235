888e2bd7b5c5b3ba6c14f1c3727608e74b444628
razeralfiano <alfianarifianto13@gmail.com>
2021-06-05 14:35:04 +0700

    commit first

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Http\Controllers\BarangController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/barang_0235','App\Http\Controllers\BarangController@index');
+Route::resource('barang',BarangController::class);
+//Route::resource('/barang', 'App\Http\Controllers\BarangController');
